df0acfddf664cba7b0a8862db2de9022d2907bcb
abdo801 <abdotm7@gmail.com>
2023-02-08 23:43:30 +0200

    c

---
+++ MyApplicationAPI/app/src/main/java/com/example/myapplicationapi/Model/post.kt
@@ -0,0 +1,12 @@
+
+
+
+@Serializable
+data class Post(
+    val id: String,
+    val title: String,
+    val content: String,
+    val slug: String,
+    val picture: String,
+    val user: String
+)
