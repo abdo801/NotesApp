e0b5b38406f1dee67a81831aec7a368b4ed19e46
abdo801 <abdotm7@gmail.com>
2023-02-20 12:08:07 +0200

    master

---
+++ Ktor-Client-Android-Sample-master (1)/Ktor-Client-Android-Sample-master/app/src/main/java/com/example/ktorsampleapp/di/AppModule.kt
@@ -0,0 +1,56 @@
+package com.example.ktorsampleapp.di
+
+import com.example.ktorsampleapp.model.PostApi
+import com.example.ktorsampleapp.model.PostApiImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideKtorClient(): HttpClient {
+
+        val client = HttpClient(Android) {
+            //Makes the requests thorow an exception if the http status code is does not start with 2
+            expectSuccess = true
+
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+                accept(ContentType.Application.Json)
+            }
+            //to map json objects returned from the api to a kotlin data class
+            install(ContentNegotiation) {
+                json(Json {
+                    //ignores json keys we have not included in our data class
+                    ignoreUnknownKeys = true
+                })
+            }
+            //a logger to see logging information about every request we make using the client
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+
+
+        }
+        return client
+    }
+    @Provides
+    @Singleton
+    fun providePostApi(client: HttpClient): PostApi {
+        return PostApiImpl(client)
+    }
+}
---
+++ Ktor-Client-Android-Sample-master (1)/Ktor-Client-Android-Sample-master/app/src/main/java/com/example/ktorsampleapp/ktorsampleapplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktorsampleapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class ktorsampleapplication: Application() {
+}
+
+
